e32e5b964bf1ab18ab19c44aad97d300ab7b3a09
Anton Aleynikov <a.aleynikov.vironit@vironit.ru>
2022-06-22 19:01:46 +0300

    follow/unfollow

--- controllers/userController.js
+++ controllers/userController.js
@@ -27,20 +27,56 @@ const userController = {
     },
     updateUser: async (req, res) => {
         try {
-            const {fullName, story, phone, address} = req.body
-            if(!fullName){
-                return res.status(500).json({msg: "Full name is required"})
+            const {avatar, fullName, story, phone, address} = req.body
+            if (!fullName) {
+                return res.status(400).json({msg: "Full name is required"})
             }
-
-            await Users.findByIdAndUpdate({_id: req.user},{
-                fullName, story, phone, address
+            await Users.findOneAndUpdate({id: req.user._id}, {
+                avatar, fullName, story, phone, address
             })
-
-            res.json({msg: "update success"})
-        } catch (err){
+            res.json({msg: "Update success"})
+        } catch (err) {
             return res.status(500).json({msg: err.message})
         }
-    }
+    },
+    follow: async (req, res) => {
+        try {
+            const user = await Users.find({_id: req.params._id, friends: req.user._id})
+            if (user.length > 0) {
+                return res.status(500).json({msg: "You have already followed"})
+            }
+
+            await Users.findOneAndUpdate({_id: req.params.id}, {
+                $push: {friends: req.user._id}
+            }, {new: true})
+
+            await Users.findOneAndUpdate({_id: req.user._id}, {
+                $push: {following: req.params.id}
+            }, {new: true})
+
+
+            res.json({msg: "You have new friend"})
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
+    },
+    unfollow: async (req, res) => {
+        try {
+
+            await Users.findOneAndUpdate({_id: req.params.id}, {
+                $pull: {friends: req.user._id}
+            }, {new: true})
+
+            await Users.findOneAndUpdate({_id: req.user._id}, {
+                $pull: {following: req.params.id}
+            }, {new: true})
+
+
+            res.json({msg: "You have new friend", user})
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
+    },
 }
 
 module.exports = userController
